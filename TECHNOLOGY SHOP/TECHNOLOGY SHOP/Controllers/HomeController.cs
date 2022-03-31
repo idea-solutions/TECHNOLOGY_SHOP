0563b477b370ecba097e63a0cacff50111a0f11a
NguyenPhuDuc307 <nguyenphuduc62001@gmail.com>
2022-03-31 16:12:26 +0700

    31/3

---
+++ TECHNOLOGY SHOP/TECHNOLOGY SHOP/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TECHNOLOGY_SHOP.Models;
+
+namespace TECHNOLOGY_SHOP.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult Index()
+        {
+            var allSanPham = from sanpham in data.tb_SanPhams select sanpham;
+            return View(allSanPham);
+        }
+    }
+}
